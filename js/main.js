778193d7eb384e61c81a43617bd3371ec84ab7be
jiohjung98 <ckaquz98@naver.com>
2023-07-12 11:09:05 +0900

    뱃지 이미지 수정

--- js/main.js
+++ js/main.js
@@ -98,7 +98,7 @@ new Swiper('.promotion .swiper-container', {
   spaceBetween: 10, // 슬라이드 사이 여백
   centeredSlides: true, // 1번 슬라이드가 가운데 보이기
   pagination: { // 페이지 번호 사용 여부
-    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자 
     clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
   },
   navigation: { // 슬라이드 이전/다음 버튼 사용 여부
